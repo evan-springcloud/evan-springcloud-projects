df9cb4148d0a312690aee2e65d525a9b59c02664
沈炜 <277469513@qq.com>
2019-08-01 16:53:32 +0800

    配置

--- config/src/main/java/org.evan.springcloud.config/ConfigServerApplication.java
+++ config/src/main/java/org.evan.springcloud.config/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package org.evan.springcloud.config;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -11,12 +13,14 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        String logDir = System.getProperty("LOG_DIR");
-        if (StringUtils.isBlank(logDir)) {
-            String userDir = System.getProperty("user.dir");
-            System.setProperty("LOG_DIR", userDir + "/log/evan-springcloud/config");
-        }
+//        String logDir = System.getProperty("LOG_DIR");
+//        if (StringUtils.isBlank(logDir)) {
+//            String userDir = System.getProperty("user.dir");
+//            System.setProperty("LOG_DIR", userDir + "/log/evan-springcloud/config");
+//        //}
 
-        SpringApplication.run(ConfigServerApplication.class, args);
+        new SpringApplicationBuilder(ConfigServerApplication.class).web(WebApplicationType.SERVLET).run(args);
+
+        //SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
--- zuul/src/main/java/org/evan/springcloud/zuul/ZuulApplication.java
+++ zuul/src/main/java/org/evan/springcloud/zuul/ZuulApplication.java
@@ -1,22 +1,25 @@
-package org.evan.springcloud.eureka;
+package org.evan.springcloud.zuul;
+
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
+ * 网关
+ *
  * @author shenwei
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
+@EnableZuulProxy
+public class ZuulApplication {
     public static void main(String[] args) {
         String logDir = System.getProperty("LOG_DIR");
         if (StringUtils.isBlank(logDir)) {
             String userDir = System.getProperty("user.dir");
-            System.setProperty("LOG_DIR", userDir + "/log/evan-springcloud/eureka");
+            System.setProperty("LOG_DIR", userDir + "/log/evan-springcloud/zuul");
         }
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 }
